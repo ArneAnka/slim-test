ddec674df2288d632536e6b3bf6e0dfe44a1139d
ArneAnka <johannilsson@JOHANs-MacBook-Pro.local>
2016-07-06 18:11:40 +0200

    CSS improvements

--- app/Middleware/GuestMiddleware.php
+++ app/Middleware/GuestMiddleware.php
@@ -7,7 +7,7 @@ class GuestMiddleware extends Middleware
     public function __invoke($request, $response, $next)
     {
         //if ($this->container->auth->check()) {
-    	if($_SESSION['user_logged_in']){
+    	if(isset($_SESSION['user_logged_in'])){
             return $response->withRedirect($this->container->router->pathFor('home'));
     	}
 
